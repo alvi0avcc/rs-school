fa396ff090a2bf1582a580325551bc4625b5759e
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2024-11-29 19:59:51 +0300

    fix: add transition to opening burger-menu

--- christmas-shop/js/header.js
+++ christmas-shop/js/header.js
@@ -27,13 +27,34 @@ function HeaderAdd(){
     header.innerHTML = data;
     
     // for burger-menu
+    // let isOpenMenu = false;
+
     const burgerButton = document.querySelector('.burger-menu');
     const navMenu = document.querySelector('.nav-menu');
+    // navMenu.classList.add('show');
 
     burgerButton.addEventListener( 'click', ()=>{
         console.log('burger :)');
-        navMenu.classList.toggle('show');
-        navMenu.classList.toggle('nav-menu-burger');
+        // console.log(navMenu.getAttribute('class'));
+        
+        // if ( navMenu.style.display == 'flex') {
+            // navMenu.classList.add('nav-menu-burger-hide');
+            // navMenu.classList.toggle('show');
+
+            navMenu.classList.toggle('nav-menu-burger');
+            navMenu.classList.toggle('show');
+
+            // navMenu.style.transition = '1s';
+            // navMenu.style.left = '50px';
+            setTimeout(()=>{
+                navMenu.classList.toggle('move');
+            },0);
+        // } else {
+            // navMenu.classList.remove('nav-menu-burger-hide');
+            // navMenu.classList.add('show');
+            // navMenu.classList.add('nav-menu-burger');
+        // }
+        
     });
     
     navMenu.addEventListener('click',(events)=>{
