c5314ba2f13c35ff5d9c062e54de4c716978ddc9
Aleksandr Vavilov <alvi.ua@gmail.com>
2024-11-30 23:35:29 +0300

    feat: load "gifts" for Gifts page from json (without sorting)

--- christmas-shop/js/data-loader.js
+++ christmas-shop/js/data-loader.js
@@ -26,24 +26,10 @@ function giftsForMain(){
 
     const giftsCards = document.getElementById('gift-cards');
     for ( let i = 0; i < 4; i++){
-        // console.log('current card = ',giftsData[lottery[i]] );
         
         const giftCard = document.createElement('div');
-        let category = '';
 
-        switch  (giftsData[lottery[i]].category) {
-            case 'For Work':
-                category = 'for-work';
-                break;
-            case 'For Health':
-                category = 'for-health';
-                break;
-            case 'For Harmony':
-                category = 'for-harmony';
-                break;
-            default:
-            console.error('error! Gift category not found');
-        };
+        const category = categoryToStyle(giftsData[lottery[i]].category);
 
         giftCard.setAttribute('id', `card-${i+1}`);
         giftCard.classList.add( "gift-card", category );
@@ -60,11 +46,50 @@ function giftsForMain(){
             giftCard.classList.add('row-center');
         }
         giftsCards.appendChild(giftCard);
-     }
+    }
 }
 
-function giftsForGifts(){
+function giftsForGifts(filter){
     const giftsLength = giftsData.length;
     console.log('page 2 giftsLength = ',giftsLength);
     
+    const giftsCards = document.getElementById('gift-cards');
+
+    for ( let i = 0; i < giftsLength; i++){
+        const giftCard = document.createElement('div');
+        const category = categoryToStyle(giftsData[i].category);
+
+        giftCard.setAttribute('id', `card-${i+1}`);
+        giftCard.classList.add( "gift-card", category );
+        const data =`
+                <div class="image-container"></div>
+                <div class="gift-description">
+                    <h4>${giftsData[i].category}</h4>
+                    <h3>${giftsData[i].name}</h3>
+                </div>
+        `;
+
+        giftCard.innerHTML = data;
+
+        giftsCards.appendChild(giftCard);
+    }
+}
+
+function categoryToStyle(category){
+    let style = '';
+    switch  (category) {
+        case 'For Work':
+            style = 'for-work';
+            break;
+        case 'For Health':
+            style = 'for-health';
+            break;
+        case 'For Harmony':
+            style = 'for-harmony';
+            break;
+        default:
+        console.error('error! Gift category not found');
+    };
+
+    return style;
 }
