32fcbe5a3c9a58a03cd8743ba5278791409c74ea
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2025-01-25 23:46:57 +0300

    feat: read "pazl" from file

---
+++ nonograms/nonograms.js
@@ -0,0 +1,35 @@
+class Nonograms {
+  constructor() {
+    
+  }
+  async loadPazlList(){
+    try {
+      const response = await fetch(`./data/list.json`);
+      if (!response.ok) {
+        throw new Error(`Error HTTP! ${response.status}`);
+      }
+      const data = await response.json();
+      console.log('list - ',data);
+    } catch (error) {
+      console.error(`Error during loading list of pazls`, error);
+    }
+  }
+
+  async loadPazlByName(name) {
+    try {
+      const response = await fetch(`./data/${name}.json`);
+      if (!response.ok) {
+        throw new Error(`Error HTTP! ${response.status}`);
+      }
+      const data = await response.json();
+      console.log('pazl - ',data);
+    } catch (error) {
+      console.error(`Error during loading pazl - ${name}:`, error);
+    }
+  }
+}
+
+const nonograms = new Nonograms();
+
+nonograms.loadPazlList();
+nonograms.loadPazlByName('x');
