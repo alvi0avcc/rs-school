b31996eb1e9ae39ecee2611b9f51fd379282d9be
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-02-21 00:23:06 +0300

    refactor: controller.ts

--- news-JS/src/components/controller/controller.ts
+++ news-JS/src/components/controller/controller.ts
@@ -13,28 +13,29 @@ class AppController extends AppLoader {
     }
 
     getNews(e: Event, callback: (data: IEverything) => void): void {
-        let target = e.target as HTMLElement;
-        const newsContainer = e.currentTarget as HTMLElement;
+        let target: HTMLElement | null = e.target as HTMLElement;
+        const newsContainer: HTMLElement | null = e.currentTarget as HTMLElement;
 
-        while (target !== newsContainer) {
-            if (target.classList.contains('source__item')) {
-                const sourceId: string | null = target.getAttribute('data-source-id');
-                if (newsContainer.getAttribute('data-source') !== sourceId) {
-                    newsContainer.setAttribute('data-source', sourceId || '');
-                    super.getResp(
-                        {
-                            endpoint: 'everything',
-                            options: {
-                                sources: sourceId || '',
+        if (target && newsContainer)
+            while (target !== newsContainer) {
+                if (target?.classList.contains('source__item')) {
+                    const sourceId: string | null = target.getAttribute('data-source-id');
+                    if (newsContainer?.getAttribute('data-source') !== sourceId) {
+                        newsContainer.setAttribute('data-source', sourceId || '');
+                        super.getResp(
+                            {
+                                endpoint: 'everything',
+                                options: {
+                                    sources: sourceId || '',
+                                },
                             },
-                        },
-                        (data: IEverything | ISources) => callback(data as IEverything)
-                    );
+                            (data: IEverything | ISources) => callback(data as IEverything)
+                        );
+                    }
+                    return;
                 }
-                return;
+                target = target.parentNode as HTMLElement;
             }
-            target = target.parentNode as HTMLElement;
-        }
     }
 }
 
