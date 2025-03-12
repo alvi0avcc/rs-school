544de2d6d593885b8049e992369fc00b189d6351
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-03-12 23:30:58 +0300

    feat: structure of main page

--- decision-making-tool/src/element-creator/element-creator.ts
+++ decision-making-tool/src/element-creator/element-creator.ts
@@ -1,7 +1,49 @@
 export default class ElementCreator {
   private defaultTag = 'div';
 
-  public Section(
+  public label(
+    tag = 'label',
+    id = '',
+    text = tag,
+    callback: EventListener | undefined = undefined
+  ): HTMLElement {
+    const label: HTMLElement = this.createElement(tag);
+    label.id = id;
+    label.textContent = text;
+    label.classList.add(tag);
+    if (callback) label.addEventListener('click', (event) => callback(event));
+    return label;
+  }
+
+  public ul(
+    tag = 'ul',
+    id = '',
+    text = '',
+    callback: EventListener | undefined = undefined
+  ): HTMLElement {
+    const ul: HTMLElement = this.createElement(tag);
+    ul.id = id;
+    ul.textContent = text;
+    ul.classList.add(tag);
+    if (callback) ul.addEventListener('click', (event) => callback(event));
+    return ul;
+  }
+
+  public li(
+    tag = 'li',
+    id = '',
+    text = '',
+    callback: EventListener | undefined = undefined
+  ): HTMLElement {
+    const li: HTMLElement = this.createElement(tag);
+    li.id = id;
+    li.textContent = text;
+    li.classList.add(tag);
+    if (callback) li.addEventListener('click', (event) => callback(event));
+    return li;
+  }
+
+  public section(
     tag = 'section',
     id = '',
     text = '',
@@ -15,15 +57,16 @@ export default class ElementCreator {
     return section;
   }
 
-  public Button(
+  public button(
     id = '',
     text = 'button',
-    callback: EventListener | undefined = undefined
+    callback: EventListener | undefined = undefined,
+    style: string[]
   ): HTMLElement {
     const button: HTMLElement = this.createElement('button');
     button.id = id;
     button.textContent = text;
-    button.classList.add('button');
+    button.classList.add(...style);
     if (callback) button.addEventListener('click', (event) => callback(event));
     return button;
   }
--- decision-making-tool/src/main/main.ts
+++ decision-making-tool/src/main/main.ts
@@ -6,7 +6,7 @@ export default class HeaderView {
   #main: HTMLElement;
   constructor() {
     this.#creator = new ElementCreator();
-    this.#main = this.#creator.Section('main');
+    this.#main = this.#creator.section('main');
     this.createMain();
   }
 
@@ -17,12 +17,56 @@ export default class HeaderView {
 
   private createMain(): HTMLElement {
     const page: HTMLElement[] = [
-      this.#creator.Button('btn1', 'Click Me', () => {
-        console.log('Button1 clicked!');
-      }),
-      this.#creator.Button('btn2', 'Click Me', () => {
-        console.log('Button2 clicked!');
-      }),
+      this.#creator.label('h1', '', 'Decision Making Tool'),
+      this.#creator.ul('ul', 'ul'),
+      this.#creator.button(
+        'btn1',
+        'Add Option',
+        () => {
+          console.log('Button1 clicked!');
+        },
+        ['button', 'add-option-button']
+      ),
+      this.#creator.button(
+        'btn2',
+        'Paste list',
+        () => {
+          console.log('Button2 clicked!');
+        },
+        ['button', 'paste-list-button']
+      ),
+      this.#creator.button(
+        'btn3',
+        'Clear list',
+        () => {
+          console.log('Button3 clicked!');
+        },
+        ['button', 'clear-list-button']
+      ),
+      this.#creator.button(
+        'btn4',
+        'Save list to file',
+        () => {
+          console.log('Button4 clicked!');
+        },
+        ['button', 'save-list-button']
+      ),
+      this.#creator.button(
+        'btn4',
+        'Load list from file',
+        () => {
+          console.log('Button4 clicked!');
+        },
+        ['button', 'load-list-button']
+      ),
+      this.#creator.button(
+        'btn5',
+        'Start',
+        () => {
+          console.log('Button5 clicked!');
+        },
+        ['button', 'start-button']
+      ),
     ];
     this.#main.append(...page);
     return this.#main;
