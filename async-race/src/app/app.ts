de335925f613cfeb521735c3bc381688375f3c75
Aleksandr Vavilov <alvi.ua@gmail.com>
2025-03-30 01:32:24 +0300

    refactor: Structure app.ts

--- async-race/src/app/app.ts
+++ async-race/src/app/app.ts
@@ -1,14 +1,18 @@
+import './app.css';
+
 import type { Route } from '../router/router';
 import { Router } from '../router/router';
 import * as create from '../builder/elements';
 
-// import GarageView from '../pages/garage';
+import { header } from '../pages/header-nav/header-nav';
+import { garage } from '../pages/garage/garage';
 // import WinnersView from '../pages/winners';
 import { notFoundView } from '../pages/404/404';
 
 const notFoundPage: Route = {
   path: '/404',
-  view: async (root) => {
+  view: (root): void => {
+    if (header) root.append(header);
     root.append(notFoundView());
   },
 };
@@ -16,31 +20,24 @@ const notFoundPage: Route = {
 const routes: Route[] = [
   {
     path: '/',
-    view: (root) => {
-      root.append(create.main({ text: 'Garage Page. Loading...' }));
-
-      //todo () => await fetchGarage();
+    view: (root): void => {
+      if (header) root.append(header);
+      root.append(...garage.getView());
     },
   },
   {
     path: '/winners',
-    view: async (root) => {
-      root.append(create.main({ text: 'Winners Page. Loading...' }));
-
-      //todo () => await fetchWinners();
+    view: (root): void => {
+      if (header) root.append(header);
+      root.append(create.section({ tag: 'main', text: 'Winners Page. Loading...' }));
     },
   },
   notFoundPage,
 ];
 
-type View = 'garage' | 'winners' | '404';
-
 export default class App {
   private root: HTMLElement;
   private router: Router;
-  // private currentView: View = 'garage';
-  // private garageView: GarageView | undefined = undefined;
-  // private winnersView: WinnersView | undefined = undefined;
 
   constructor() {
     this.root = document.body;
@@ -48,35 +45,6 @@ export default class App {
   }
 
   public init(): void {
-    console.log('app initialized');
-    // this.switchView(this.currentView);
-  }
-
-  private switchView(view: View) {
-    // this.currentView = view;
-    //todo clear current dom view
-
-    switch (view) {
-      case 'garage': {
-        break;
-      }
-
-      case 'winners': {
-        break;
-      }
-
-      case '404': {
-        break;
-      }
-
-      default: {
-        break;
-      }
-    }
-    if (view === 'garage') {
-      // this.garageView = new GarageView();
-    } else {
-      // this.winnersView = new WinnersView();
-    }
+    this.router = new Router(this.root, routes, notFoundPage);
   }
 }
